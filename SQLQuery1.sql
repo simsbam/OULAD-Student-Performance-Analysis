a7bea1da8698df1f1cd7c2fcc634be86e99a66d6
Sims <bamgboyesimisola@gmail.com>
2025-09-24 12:34:54 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,22 @@
+Create database StudentVle;
+
+Create Database OULAD;
+
+Select * from dbo.studentVle_processed;
+
+Select * from dbo.studentAssessment ; 
+
+SELECT *
+FROM studentAssessment
+WHERE score IS NULL;
+
+SELECT id_assessment, id_student, COALESCE(score, 0) AS score
+FROM studentAssessment
+WHERE score IS NULL;
+
+UPDATE studentAssessment
+SET score = '0'
+Where score IS NULL;
+
+
+SELECT * FROM StudentInfo;
